1302033d6a692a0a79a3c2f2c244f54ffae83aa7
BlackZone017 <20175184@itla.edu.do>
2019-03-22 17:06:18 -0400

    First Commit

---
+++ HTMLHelpers/HTMLHelpers/Models/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HTMLHelpers.Models
+{
+    public class Persona
+    {
+        [Required]
+        public string cedula { get; set; }
+
+        [Required]
+        public string nombre { get; set; }
+
+        [Required]
+        public string apellido { get; set; }
+
+        [Range(15,99)]
+        public int edad { get; set; }
+
+        public string telefono { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string correo  { get; set; }
+
+        public Genero genero { get; set; }
+
+        public string EstadoCivil { get; set; }
+
+        public string[] Hobbys { get; set; }
+
+    }
+
+    public enum Genero
+    {
+        Hombre,
+        Mujer
+    }
+
+}
